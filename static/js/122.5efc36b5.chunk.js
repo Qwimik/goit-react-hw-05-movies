94cbdbc7673156aecffbd3b9247b70c21ad2ef8f
Qwimik <109024305+Qwimik@users.noreply.github.com>
2023-03-23 20:42:06 +0000

    Deploying to gh-pages from @ Qwimik/goit-react-hw-05-movies@858f36dfa8e3b85603986d2e22dc15ae9eb6aeea 🚀

---
+++ static/js/122.5efc36b5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[122],{122:function(e,t,i){i.r(t),i.d(t,{default:function(){return r}});var n=i(184);var r=function(){return(0,n.jsx)("div",{children:"Reviews"})}}}]);
+//# sourceMappingURL=122.5efc36b5.chunk.js.map
---
+++ static/js/877.935e526c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[877],{877:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var r=n(184);var i=function(){return(0,r.jsx)("div",{children:"Cast"})}}}]);
+//# sourceMappingURL=877.935e526c.chunk.js.map
